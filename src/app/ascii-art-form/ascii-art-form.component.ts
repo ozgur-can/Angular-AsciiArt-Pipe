287b4bda5e0a0e562557edf987f09e68713b38ed
Özgür Can Altınok <ozgurcanaltinok@gmail.com>
2022-08-08 15:43:29 +0300

    Add ascii art input & output UI
     - update pipe
    - add some fonts for selection
    - add tailwind

--- src/app/ascii-art-form/ascii-art-form.component.ts
+++ src/app/ascii-art-form/ascii-art-form.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-ascii-art-form',
   templateUrl: './ascii-art-form.component.html',
   styleUrls: ['./ascii-art-form.component.css']
 })
+
+
 export class AsciiArtFormComponent implements OnInit {
   text: string = '';
-
+  fontName: string = 'Standard';
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onFontChange(event: Event) {
+    //@ts-ignore
+    const selectedFontName = event.target.value;
+    this.fontName = selectedFontName; 
+  }
 }
--- src/app/shared/ascii-art.pipe.ts
+++ src/app/shared/ascii-art.pipe.ts
@@ -1,21 +1,47 @@
 // @ts-nocheck
 import { Pipe, PipeTransform } from '@angular/core';
 import figlet from 'figlet';
-// import standardFont from 'figlet/importable-fonts/Standard.js';
-import standardFont from 'figlet/importable-fonts/Isometric3.js';
-
+import Standard from 'figlet/importable-fonts/Standard.js';
+import Isometric3 from 'figlet/importable-fonts/Isometric3.js';
+import Fuzzy from 'figlet/importable-fonts/Fuzzy.js';
+import Fraktur from 'figlet/importable-fonts/Fraktur.js';
+import Whimsy from 'figlet/importable-fonts/Whimsy.js';
 @Pipe({
-  name: 'asciiart'
+  name: 'asciiart',
 })
 export class AsciiArtPipe implements PipeTransform {
-  transform(value: string) {
+  transform(value: string, fontName: string) {
+    let selectedFont: string = null;
+
+    // font selection
+    switch (fontName) {
+      case 'Standard':
+        selectedFont = Standard;
+        break;
+      case 'Isometric3':
+        selectedFont = Isometric3;
+        break;
+      case 'Fuzzy':
+        selectedFont = Fuzzy;
+        break;
+      case 'Fraktur':
+        selectedFont = Fraktur;
+        break;
+      case 'Whimsy':
+        selectedFont = Whimsy;
+        break;
+
+      default:
+        break;
+    }
+
     // load font for figlet package
-    figlet.parseFont('Isometric3', standardFont);
+    figlet.parseFont(fontName, selectedFont);
 
     // initial value for ascii
     let asciiArt: string = '';
 
-    figlet.text(value, { font: 'Isometric3' }, (err, data) => {
+    figlet.text(value, { font: fontName }, (err, data) => {
       if (err) {
         throw new Error(
           'problem occured while transforming text to ascii art!'
